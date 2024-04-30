f850b5158b73d93ec53169ad15065c2e16114c88
AnnaLobunova <alobunova@gmail.com>
2024-04-30 22:28:02 +0300

    changes to Problem 1 and Problem 2 made

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 "use strict"
 //Problem 1
-// function iterativeOddSumTo(number){
-//     let sum = 0;
-//     for(let i=1; i<=number; i++){
-//         i%2===0 ? sum+=0 : sum+=i;
-//     }
-//     return sum;
-// }
-// console.log(iterativeOddSumTo(1)) ;
-// console.log(iterativeOddSumTo(9)) ;
-// console.log(iterativeOddSumTo(20));
+function iterativeOddSumTo(number){
+    let sum = 0;
+    for(let i=1; i<=number; i++){
+        if (i%2) {
+            sum+=i;
+        };
+    }
+    return sum;
+}
+console.log(iterativeOddSumTo(1)) ;
+console.log(iterativeOddSumTo(9)) ;
+console.log(iterativeOddSumTo(10));
 
 //Problem 2
 // function recursiveOddSumTo(number) {
 // if (number <=1){
 // return number;
-// } else if (number%2 !== 0) {
+// } else if (number % 2) {
 //     return number + recursiveOddSumTo(number-2);
 // }
 // else {
@@ -28,23 +30,23 @@
 
 //Problem 3
 
-const isXOEqual = (str) => {
-    const lowerStr = str.toLowerCase();
-    let numX=0;
-    let numO = 0;
-for (let i=0; i<=str.length; i++){
-    if (lowerStr[i]==='x'){
-        numX++;
-    } else if(lowerStr[i]==='o'){
-        numO++;
-    } else {
-    }
-};
- return numO === numX 
-}
+// const isXOEqual = (str) => {
+//     const lowerStr = str.toLowerCase();
+//     let numX=0;
+//     let numO = 0;
+// for (let i=0; i<=str.length; i++){
+//     if (lowerStr[i]==='x'){
+//         numX++;
+//     } else if(lowerStr[i]==='o'){
+//         numO++;
+//     } else {
+//     }
+// };
+//  return numO === numX 
+// }
 
-console.log(isXOEqual("ooxx")) 
-console.log(isXOEqual("xooxx"))
-console.log(isXOEqual("ooxXm"))
-console.log(isXOEqual("zpzpzpp"))
-console.log(isXOEqual("zzoo"))
+// console.log(isXOEqual("ooxx")) 
+// console.log(isXOEqual("xooxx"))
+// console.log(isXOEqual("ooxXm"))
+// console.log(isXOEqual("zpzpzpp"))
+// console.log(isXOEqual("zzoo"))
